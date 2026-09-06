413fb6c377071ac818b3ec87fbede3949d149710
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-09-06 12:14:15 +0300

    implement forget password page

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -2,15 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { Router } from '@angular/router';
 
 @Service()
 export class AuthService {
   private readonly httpClient = inject(HttpClient);
+  private readonly router = inject(Router);
 
   signUp(data: object): Observable<any> {
     return this.httpClient.post<any>(environment.baseUrl + '/auth/signup', data);
   }
+
   signin(data: object): Observable<any> {
     return this.httpClient.post<any>(environment.baseUrl + '/auth/signin', data);
+  }
+
+  forgetPassword(data: object): Observable<any> {
+    return this.httpClient.post<any>(environment.baseUrl + '/auth/forgotPasswords', data);
+  }
+  verifyEmail(data: object): Observable<any> {
+    return this.httpClient.post<any>(environment.baseUrl + '/auth/verifyResetCode', data);
+  }
+  resetPassword(data: object): Observable<any> {
+    return this.httpClient.put<any>(environment.baseUrl + '/auth/resetPassword', data);
+  }
+
+  logout() {
+    localStorage.removeItem('freshToken');
+    localStorage.removeItem('freshUser');
+    this.router.navigate(['/login']);
   }
 }
--- src/app/features/auth/forget/forget.component.ts
+++ src/app/features/auth/forget/forget.component.ts
@@ -1,8 +1,74 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '@core/auth/services/auth.service';
 
 @Component({
-  imports: [],
+  imports: [RouterLink, NgClass, ReactiveFormsModule],
   selector: 'app-forget',
   templateUrl: './forget.component.html',
 })
-export class ForgetComponent {}
+export class ForgetComponent {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  step = signal(1);
+
+  email = new FormControl('', [Validators.required]);
+  code = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
+
+  submitEmailHandler(event: Event) {
+    event.preventDefault();
+    if (this.email.valid) {
+      // send data to backend
+      this.authService.forgetPassword({ email: this.email.value }).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.step.set(2);
+        },
+      });
+    }
+  }
+
+  submitCodeHandler(event: Event) {
+    event.preventDefault();
+    if (this.code.valid) {
+      // send data to backend
+      this.authService.verifyEmail({ code: this.code.value }).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.step.set(3);
+        },
+      });
+    }
+  }
+
+  submitPasswordHandler(event: Event) {
+    event.preventDefault();
+    if (this.password.valid) {
+      // send data to backend
+      const data = {
+        email: this.email.value,
+        newPassword: this.password.value,
+      };
+      this.authService.resetPassword(data).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.step.set(1);
+          // navigate
+          this.router.navigate(['/login']);
+        },
+      });
+    }
+  }
+
+  resendCodeHandler() {
+    this.authService.forgetPassword({ email: this.email.value }).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+    });
+  }
+}
