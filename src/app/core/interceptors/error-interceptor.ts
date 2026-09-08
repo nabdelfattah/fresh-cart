86c675377e855c5f67a9586cb67f9d3508d6886b
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-09-08 16:23:22 +0300

    fix server error

--- src/app/core/interceptors/error-interceptor.ts
+++ src/app/core/interceptors/error-interceptor.ts
@@ -1,18 +1,22 @@
+import { isPlatformBrowser } from '@angular/common';
 import { HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { catchError, throwError } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const toastrService = inject(ToastrService);
+  const platformId = inject(PLATFORM_ID);
 
   return next(req).pipe(
     catchError((err) => {
-      toastrService.error(err.error.message, 'FreshCart', {
-        progressBar: true,
-        closeButton: true,
-        timeOut: 6000,
-      });
+      if (isPlatformBrowser(platformId)) {
+        toastrService.error(err.error.message, 'FreshCart', {
+          progressBar: true,
+          closeButton: true,
+          timeOut: 6000,
+        });
+      }
       return throwError(() => err);
     }),
   );
